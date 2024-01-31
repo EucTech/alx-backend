d4a6c2cd992c7576b76bd57094abcc706028c789
EucTech <euccreative@gmail.com>
2024-01-31 18:39:54 +0100

    0-basic_cache.py

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Create a class BasicCache that inherits from BaseCaching
+and is a caching system"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ Basecache class"""
+    def put(self, key, item):
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
