4da9a97c34d6606049cda247bbe629747df787c5
EucTech <euccreative@gmail.com>
2024-01-31 18:41:25 +0100

    0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -8,10 +8,12 @@ from base_caching import BaseCaching
 class BasicCache(BaseCaching):
     """ Basecache class"""
     def put(self, key, item):
+        """put function"""
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
+        """get function"""
         if key is not None and key in self.cache_data:
             return self.cache_data[key]
         return None
