16dba7734fbcdac62f4b2409592cfacee4322af8
EucTech <euccreative@gmail.com>
2024-02-06 23:29:28 +0100

    1-app.py

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Then instantiate the Babel object in your app.
+Store it in a module-level variable named babel"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+Babel = Babel(app)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route("/")
+def index():
+    """home page"""
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
